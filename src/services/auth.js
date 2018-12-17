53389c06c2b9335de064183de5f17977ddaf127e
Stefano Antonel <stefano.antonel@epfl.ch>
2018-12-17 15:54:04 +0100

    Remove access_token from url
    Change-Id: Idb387c932ec9ba76d25f48524bc4ee1f299f5a98

--- src/services/auth.js
+++ src/services/auth.js
@@ -16,6 +16,19 @@ const client = new JSO({
 function init() {
   client.callback();
 
+  /*
+   * check if the access_token is in the URL and remove it to avoid
+   * going to another page that consider access_token as param
+   */
+  if (window.location.href.includes('access_token')) {
+    /* eslint-disable no-console */
+    console.debug('URL has token, removing it ...');
+    /* eslint-enable no-console */
+    const url = window.location.href;
+    const accessTokenIndex = url.indexOf('%2F&access_token') || url.indexOf('access_token');
+    window.location.href = url.substr(0, accessTokenIndex);
+  }
+
   const authorization = client.getToken();
   authorization.then((session) => {
     store.commit('setToken', session.access_token);

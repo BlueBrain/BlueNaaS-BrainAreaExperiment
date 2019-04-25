9f2bdff5c513ab2afe461728b4986cc8257f831a
Stefano Antonel <stefano.antonel@epfl.ch>
2019-04-25 14:05:22 +0200

    Upgrade Sentry initial configuration with integrations
    Change-Id: I97aa50d1763eb307785a5b3823e4edfe7be2d3fd

--- src/common/sentry.js
+++ src/common/sentry.js
@@ -1,12 +1,18 @@
 
 import Vue from 'vue';
 import * as Sentry from '@sentry/browser';
+import * as Integrations from '@sentry/integrations';
 
 const sentryDsn = process.env.VUE_APP_SENTRY_DSN;
 
 if (sentryDsn) {
   Sentry.init({
     dsn: sentryDsn,
-    integrations: [new Sentry.Integrations.Vue({ Vue })],
+    integrations: [
+      new Integrations.Vue({
+        Vue,
+        attachProps: true,
+      }),
+    ],
   });
 }

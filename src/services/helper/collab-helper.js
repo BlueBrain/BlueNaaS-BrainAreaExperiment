9fec52c0fc1a5e03d2299308ac390cce804d31da
Stefano Antonel <stefano.antonel@epfl.ch>
2019-05-23 09:56:31 +0200

    Fix issue with fetching viz ip from Collab
    Change-Id: Ie0b4721615b0d343b6d62dc5132710aaa34511df

--- src/services/helper/collab-helper.js
+++ src/services/helper/collab-helper.js
@@ -1,24 +1,16 @@
 
-import axios from 'axios';
+import { axiosInstance } from '@/services/unicore';
 
 const collabStorageEndpoint = 'https://services.humanbrainproject.eu/storage/v1/api';
 
 async function getIpByName(collabId, fileName) {
-  const newAxiosInstance = axios.create({
-    headers: {
-      common: {
-        Accept: '*/*',
-      },
-    },
-  });
-
   const fileUrl = `${collabStorageEndpoint}/entity/?path=/${collabId}/${fileName}`
     .replace(/\s+/g, '');
 
-  const fileResponse = await newAxiosInstance(fileUrl);
+  const fileResponse = await axiosInstance(fileUrl);
   const fileUuid = fileResponse.data.uuid;
 
-  const content = await newAxiosInstance(`${collabStorageEndpoint}/file/${fileUuid}/content/`);
+  const content = await axiosInstance(`${collabStorageEndpoint}/file/${fileUuid}/content/`);
   return content.data || null;
 }
 

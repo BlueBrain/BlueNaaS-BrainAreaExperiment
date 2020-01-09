a0b123ec627962fad87a9b600170b08424ee681c
Stefano Antonel <stefano.antonel@epfl.ch>
2020-01-09 14:15:42 +0100

    Check expiration token
    Change-Id: Ib70ea21858bc48ddeae5677b08f9ad0756f9e284

--- src/services/auth.js
+++ src/services/auth.js
@@ -42,8 +42,8 @@ function init() {
 
   const authorization = client.getToken();
   authorization.then((session) => {
-    const nowTime6Char = parseInt(Date.now().toString().substr(0, 6), 10);
-    const expiration6Char = parseInt(session.expires.toString().substr(0, 6), 10);
+    const nowTime6Char = parseInt(Date.now().toString().substr(0, 5), 10);
+    const expiration6Char = parseInt(session.expires.toString().substr(0, 5), 10);
     if (expiration6Char <= nowTime6Char) { // makes sure to reload the token if expires
       client.wipeTokens();
       window.location.reload();

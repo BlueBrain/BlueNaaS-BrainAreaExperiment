9b957782d8bfbebe8b4ffd26949e4c3a0ec54df4
Stefano Antonel <stefanoantonel@gmail.com>
2022-06-29 12:02:54 +0200

    Merge projection-config

--- src/config/projection-config.js
+++ src/config/projection-config.js
@@ -75,29 +75,6 @@ const hippocampusMoocProjections = Object.assign(
   },
 );
 
-const sscxProjections = {
-  defaultProjection: hippocampusProjections.defaultProjection,
-  projectionBlock: {
-    projectionSrcTarget: 'proj_Thalamocortical_VPM_Source',
-    Projection: {
-      Thalamocortical_input_VPM: {
-        Path: '/gpfs/bbp.cscs.ch/project/proj64/circuits/O1.v6a/20171212/proj_Thalamocortical_VPM/20171214-2/',
-        Source: 'proj_Thalamocortical_VPM_Source',
-      },
-    },
-    Connection: {
-      scheme_CaUse_ee_tc2c: {
-        Source: 'proj_Thalamocortical_VPM_Source',
-        Destination: 'Mosaic',
-        // SpontMinis, // added if spontMinis proj was selected
-        Weight: '1', // this will be changed by the user
-        SynapseConfigure: '%s.Use *= 0.158401372855',
-      },
-    },
-    spikeReplay: hippocampusProjections.projectionBlock.spikeReplay,
-  },
-};
-
 const sscxMouseProjections = {
   defaultProjection: hippocampusProjections.defaultProjection,
   projectionBlock: {
@@ -122,23 +99,17 @@ const sscxMouseProjections = {
 };
 
 const projectionsConfigMapping = {
-  [computers.JURECA]: {
-    [circuits.HIPPO_HBP_MICROCIRCUIT]: Object.assign({}, hippocampusProjections),
-    [circuits.HIPPO_HBP_FULL_CA1]: Object.assign({}, hippocampusProjections),
-  },
   [computers.PIZ_DAINT]: {
     [circuits.HIPPO_HBP_MICROCIRCUIT]: Object.assign({}, hippocampusProjections),
     [circuits.HIPPO_HBP_FULL_CA1]: Object.assign({}, hippocampusProjections),
-  },
-  [computers.BB5]: {
-    [circuits.HIPPO_BBP_FULL_CA1]: Object.assign({}, hippocampusProjections),
-    [circuits.HIPPO_BBP_MICROCIRCUIT]: Object.assign({}, hippocampusProjections),
-    [circuits.SSCX_BBP_MICROCIRCUIT]: Object.assign({}, sscxProjections),
   },
   [computers.SERVICE_ACCOUNT]: {
     [circuits.HIPPO_MOOC_SA_MICROCIRCUIT]: Object.assign({}, hippocampusMoocProjections),
     [circuits.HIPPO_HBP_SA_FULL_CA1]: Object.assign({}, hippocampusProjections),
     [circuits.SSCX_HBP_SA_MOUSE_MICROCIRCUIT]: Object.assign({}, sscxMouseProjections),
+  },
+  [computers.BB5_MOOC]: {
+    [circuits.BB5_MOOC]: Object.assign({}, hippocampusMoocProjections),
   },
 };
 

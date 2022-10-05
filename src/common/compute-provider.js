f8cdbb2db96a7a5b2e705dc71fdf61b8602b7498
Stefano Antonel <stefanoantonel@gmail.com>
2022-10-05 16:17:57 +0200

    Fix details redirect after launching sim

--- src/common/compute-provider.js
+++ src/common/compute-provider.js
@@ -1,20 +1,22 @@
 
+import { computers } from '@/common/constants';
+
 const bb5VmmUrl = process.env.VUE_APP_VMM_URL;
 
 const config = {
   BB5_MOOC: {
     name: 'MOOC BB5 Service Account',
-    id: 'bb5',
+    id: computers.BB5_MOOC,
     url: `${bb5VmmUrl}/rest/core`,
   },
   PIZ_DAINT: {
     name: 'PIZ_DAINT',
-    id: 'piz_daint',
+    id: computers.PIZ_DAINT,
     url: 'https://brissago.cscs.ch:8080/DAINT-CSCS/rest/core',
   },
   SERVICE_ACCOUNT: {
     name: 'MOOC Piz Daint Service Account',
-    id: 'service_account',
+    id: computers.SERVICE_ACCOUNT,
     url: 'https://bspsa.cineca.it/advanced/pizdaint/rest/core',
   },
 };
